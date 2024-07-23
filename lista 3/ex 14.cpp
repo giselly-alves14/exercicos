c46f95206d228ec03e45795a518aac7d00271003
giselly-alves14 <162717548+giselly-alves14@users.noreply.github.com>
2024-07-23 15:14:39 -0300

    Update ex 14.cpp
    ta errado, o certo é o 14.2

--- lista 3/ex 14.cpp
+++ lista 3/ex 14.cpp
@@ -1,38 +1,4 @@
-#include<stdio.h>
-int main () {
-	 float salario,bonus, anos;
-	 char faltas[4];
-	
-	printf("insira seu salario\n");
-	scanf("%f", & salario);
-	
-	printf("insira seus anos trabalhados na empresa\n");
-	scanf("%f", anos);
-	
-	//while getchar pois mudei a variavel para um caracter
-		while ( getchar () != '\n');
 
-	printf("voce possui alguma falta?");
-	fgets(faltas,sizeof(faltas),stdin);
-
-	if((anos<=5) && (strcmp(faltas, "sim")==0)){
-		bonus=salario*0.10;
-		
-	} else if((anos>5) && (strcmp(faltas, "nao")==0)){
-		bonus=salario*0.25;
-		
-	} else if((anos<=5) && (strcmp(faltas, "nao")==0)){
-		bonus=salario*0.15;
-		
-	} else if((anos>5) && (strcmp(faltas,"sim")==0)){
-		bonus=salario*0.20;
-		
-	} else{
-		printf("nao foi possivel calcular seu bonus");
-		
-		
-	}
-	printf("seu bonus é: %.3f \n", bonus);
 	
 	
 	
